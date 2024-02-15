001dad8aee38d0eefe4dd2e80a157d377ba7b8db
fanb12 <fantaneshbezabh2@gmail.com>
2024-02-16 00:45:52 +0300

    frontend deploy done

--- src/component/LowerHeader/LowerHeader.jsx
+++ src/component/LowerHeader/LowerHeader.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { CiMenuBurger } from "react-icons/ci";
 import classes from "./header.module.css";
-import { Link } from "react-router-dom";
 
 function LowerHeader() {
   return (
     <div className={classes.lower_container}>
       <ul>
-        <Link to="/menu">
+        <li>
           <CiMenuBurger />
           <p>All</p>
-        </Link>
+        </li>
         <li>Today's Deals</li>
         <li>Costomer Service</li>
         <li>Registry</li>
